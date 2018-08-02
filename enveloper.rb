c7a328acfcb3fc8482b5531698a9ae3fc8c327bd
Vlad Stoian <vstoian@pivotal.io>
2018-08-02 16:10:00 +0100

    Have an exec filter working

---
+++ enveloper.rb
@@ -0,0 +1,31 @@
+require 'json'
+
+begin
+  while line = STDIN.gets # continue to read a event from stdin
+    line.chomp!
+
+    # Input format depends on exec_filter's in_format setting
+    json = JSON.parse(line)
+
+    # main processing. You can do anything, mutate record, access to database and etc.
+
+    enveloped_json = {
+      'envelope' => {
+        "product_version" => "1.2.3",
+        "cloud_provider" => "GCP",
+        "vcenter_id" => "1234-abcd"
+      },
+      'payload' => {
+        "eventType" => "create_cluster",
+        "data" => json
+      }
+    }
+
+    # Write data to stdout. Output format depends on exec_filter's out_format setting
+    STDOUT.print JSON.dump(enveloped_json)
+
+    # Call flush to avoid buffering events
+    STDOUT.flush
+  end
+rescue Interrupt # Ignore Interrupt exception because it happens during exec_filter shutdown
+end
--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-# bosh -d fluentd deploy fluentd-on-bosh.yml
+bosh -d fluentd-test deploy fluentd-on-bosh.yml
+bosh -d fluentd-test scp enveloper.rb :/tmp/ || true
 
 kubectl delete -f fluent-bit-on-k8s-deployment.yml
 kubectl apply -f fluent-bit-on-k8s-deployment.yml
